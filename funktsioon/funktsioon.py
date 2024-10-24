232d3319eedb91e446a47c6cb58d035a9e8ac1f5
madlad20 <chrislucahomenja@gmail.com>
2024-10-24 13:09:33 +0300

    funtsioon.py

---
+++ funktsioon/funktsioon.py
@@ -0,0 +1,13 @@
+def print_hello():
+    print("Hello")
+print_hello()
+def get_hello():
+    return "Hello"
+print (get_hello)
+def ask_name_and_greet_user():
+    name = input("What is your name? ").strip().capitalize()
+    
+    print ("Get out of here, Thanos! Nobody wants to play with you!")
+    
+    print (f"Hi, {capitalized_name}. Would you like to have a Hamburger?")
+    
