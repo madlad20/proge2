52f51e16564d799b569f4aaf807ce849d2184abf
madlad20 <chrislucahomenja@gmail.com>
2024-11-07 09:49:26 +0200

    Sõne.py

---
+++ Sõne.py
@@ -0,0 +1,10 @@
+first_name = "James"
+last_name = "Bond"
+full_name = first_name + " " + last_name
+self_description_sentence = f"My name is {last_name}, {first_name} {last_name}."
+cake = "vahukoormarjadtäidispõhi"
+print("vahukoor\nmarjad\ntäidis\npõhi")
+original_string = "Programming is fun!"
+backwards = "!nuf si gnimmargorP"
+every_other = original_string [::2]
+first_word_reversed = original_string.split () [0] [::-1]
